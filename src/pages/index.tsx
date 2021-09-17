ff365d67a2bfe925c77314a712b05c8e31a4f913
Yuri Lopes Machado <yurilopesmachado@hotmail.com>
2021-09-17 09:02:46 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => { router.push('/charts')}, [])
+  
   return (
     <>
       <Head>
